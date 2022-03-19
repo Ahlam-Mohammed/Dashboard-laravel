b03982f7cdb34ba60ae12ddf59dd6ca6bc26e3f2
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-03-19 18:04:20 +0300

    first commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -339,4 +339,4 @@
 
     </body>
     @include('layouts.simple.script')
-</html>
+</html> --}}
